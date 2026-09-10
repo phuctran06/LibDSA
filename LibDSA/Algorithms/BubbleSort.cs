ed20d426eddbca36a59a3e3815f89520db983b86
phuctran06 <phuc.tran.it06@gmail.com>
2026-09-10 19:46:45 +0700

    Update BubbleSort

---
+++ LibDSA/Algorithms/BubbleSort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static LibDSA.DSAHelper;
+
+namespace LibDSA
+{
+    public partial class Sort
+    {
+        public void BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1]) 
+                    {
+                        Swap(ref arr[j], ref arr[j + 1]); 
+                    }
+                }
+            }
+        }
+    }
+}
