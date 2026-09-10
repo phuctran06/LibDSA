4ae55673daaa925d3c052f490dbe1024f8c04d08
phuctran06 <phuc.tran.it06@gmail.com>
2026-09-10 19:53:40 +0700

    Update Project configuration and Helper

---
+++ LibDSA/Algorithms/SelectionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static LibDSA.DSAHelper;
+
+namespace LibDSA
+{
+    public partial class Sort
+    {
+        public void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                int min_index = i;
+
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min_index] )
+                    {
+                        min_index = j;
+                    }
+                }
+                if (arr[i] > arr[min_index]) Swap(ref arr[i], ref arr[min_index]);
+
+
+            }
+        }
+    }
+}
---
+++ LibDSA/Helper/DSAHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibDSA
+{
+    public class DSAHelper
+    {
+        public static void Swap(ref int a,ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+            
+        }
+    }
+}
